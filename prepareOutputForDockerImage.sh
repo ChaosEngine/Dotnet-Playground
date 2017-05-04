69a6a888f60c58c9a5154b18486b23092dd9f9e8
ChaosEngine <andrzej.pauli@gmail.com>
2017-05-04 23:27:25 +0200

    moved files up diretory structure

--- prepareOutputForDockerImage.sh
+++ prepareOutputForDockerImage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-docker run -it --rm -v $(pwd):/app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore && dotnet publish -c Release && find bin/Release/netcoreapp*/publish/ -type d -exec chmod u=rwx,g=rwx,o=rx {} \;; find bin/Release/netcoreapp*/publish/ wwwroot/{js,css}/ -type f -exec chmod u=rw,g=rw,o=r {} \;" \
+docker run -it --rm -v $(pwd):/app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore AspNetCore.ExistingDb && dotnet publish -c Release && find AspNetCore.ExistingDb/bin/Release/netcoreapp*/publish/ -type d -exec chmod u=rwx,g=rwx,o=rx {} \;; find AspNetCore.ExistingDb/bin/Release/netcoreapp*/publish/ AspNetCore.ExistingDb/wwwroot/{js,css}/ -type f -exec chmod u=rw,g=rw,o=r {} \;" \
 && docker build -t chaosengine/aspnetcore . \
 && chmod 777 shared
