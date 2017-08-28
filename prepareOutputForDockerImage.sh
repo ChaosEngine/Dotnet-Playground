140d9bb4143e0f30a9534cfc712a2b0bc0a71e2d
ChaosEngine <andrzej.pauli@gmail.com>
2017-08-29 00:25:36 +0200

    publishing only main project (not tests)

--- prepareOutputForDockerImage.sh
+++ prepareOutputForDockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-docker run -it --rm -v $(pwd):/app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore AspNetCore.ExistingDb && dotnet publish -c Release && find AspNetCore.ExistingDb/bin/Release/netcoreapp*/publish/ -type d -exec chmod u=rwx,g=rwx,o=rx {} \;; find AspNetCore.ExistingDb/bin/Release/netcoreapp*/publish/ AspNetCore.ExistingDb/wwwroot/{js,css}/ -type f -exec chmod u=rw,g=rw,o=r {} \;" \
+docker run -it --rm -v $(pwd):/app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore AspNetCore.ExistingDb && dotnet publish -c Release AspNetCore.ExistingDb && find AspNetCore.ExistingDb/bin/Release/netcoreapp*/publish/ -type d -exec chmod u=rwx,g=rwx,o=rx {} \;; find AspNetCore.ExistingDb/bin/Release/netcoreapp*/publish/ AspNetCore.ExistingDb/wwwroot/{js,css}/ -type f -exec chmod u=rw,g=rw,o=r {} \;" \
 && docker build -t chaosengine/aspnetcore:2.0 . \
 && chmod 777 shared
 #
