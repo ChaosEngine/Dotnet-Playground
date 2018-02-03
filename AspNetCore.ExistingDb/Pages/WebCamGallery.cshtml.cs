b5fca7de61b4fdda5a19178cf2c3af14d8f58a98
ChaosEngine <andrzej.pauli@gmail.com>
2018-02-03 21:42:38 +0100

    WebCamGallery

---
+++ AspNetCore.ExistingDb/Pages/WebCamGallery.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace EFGetStarted.AspNetCore.ExistingDb.Models
+{
+	public class WebCamGallery : PageModel
+	{
+		private readonly string _imageDirectory;
+
+		public IEnumerable<string> Jpgs { get; private set; }
+
+		public string LiveWebCamURL { get; private set; }
+
+		public WebCamGallery(IConfiguration configuration)
+		{
+			var imageDirectory = configuration["ImageDirectory"];
+			_imageDirectory = imageDirectory;
+			LiveWebCamURL = configuration["LiveWebCamURL"];
+		}
+
+		public void OnGet()
+		{
+			if (Directory.Exists(_imageDirectory))
+			{
+				var di = new DirectoryInfo(_imageDirectory);
+				var files = di.EnumerateFiles("*.jpg", SearchOption.TopDirectoryOnly);
+
+				Jpgs = files.OrderByDescending(f => f.LastWriteTime).Select(x => x.Name);
+			}
+		}
+	}
+}
---
+++ AspNetCore.ExistingDb/Pages/WebCamImages.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+
+namespace EFGetStarted.AspNetCore.ExistingDb.Models
+{
+	public class WebCamImagesModel : PageModel
+	{
+		public IActionResult OnGet([FromServices]IConfiguration configuration, string fileName)
+		{
+			if (string.IsNullOrEmpty(fileName)) return NotFound();
+
+			var imageDirectory = configuration["ImageDirectory"];
+			var path = Path.Combine(imageDirectory, fileName);
+			if (System.IO.File.Exists(path))
+			{
+				if (Path.GetDirectoryName(path) == imageDirectory)
+				{
+					var fi = new FileInfo(path);
+					var length = fi.Length;
+					DateTimeOffset last = fi.LastWriteTime;
+					// Truncate to the second.
+					var lastModified = new DateTimeOffset(last.Year, last.Month, last.Day, last.Hour, last.Minute, last.Second, last.Offset).ToUniversalTime();
+
+					long etagHash = lastModified.ToFileTime() ^ length;
+					var etag_str = '\"' + Convert.ToString(etagHash, 16) + '\"';
+
+					string incomingEtag = (Request.Headers as FrameRequestHeaders).HeaderIfNoneMatch;
+
+					if (String.Compare(incomingEtag, etag_str) == 0)
+					{
+						Response.Clear();
+						Response.StatusCode = (int)System.Net.HttpStatusCode.NotModified;
+						return new StatusCodeResult((int)System.Net.HttpStatusCode.NotModified);
+					}
+					var etag = new EntityTagHeaderValue(etag_str);
+
+					//var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+					PhysicalFileResult pfr = base.PhysicalFile(path, "image/jpg");
+					pfr.EntityTag = etag;
+
+					return pfr;
+				}
+			}
+			return NotFound();
+		}
+	}
+}
