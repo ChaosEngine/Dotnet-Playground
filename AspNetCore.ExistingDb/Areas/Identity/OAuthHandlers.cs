1d805913afe3cac848bab70f72b9101fce2abf1b
ChaosEngine <andrzej.pauli@gmail.com>
2019-01-11 01:20:32 +0100

    Logout changes (ome fixups)authentication/authorization reworking (abstracting away)

--- AspNetCore.ExistingDb/Areas/Identity/OAuthHandlers.cs
+++ AspNetCore.ExistingDb/Areas/Identity/OAuthHandlers.cs
@@ -1,5 +1,4 @@
 ﻿using IdentitySample.DefaultUI.Data;
-using InkBall.Module;
 using InkBall.Module.Hubs;
 using InkBall.Module.Model;
 using Microsoft.AspNetCore.Authentication;
@@ -9,14 +8,10 @@ using Microsoft.AspNetCore.Authentication.Twitter;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Globalization;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
@@ -140,121 +135,19 @@ namespace AspNetCore.ExistingDb.Helpers
 		{
 			var principal = await base.CreateUserPrincipalAsync(user);
 
-			// use this.UserManager if needed
-			var identity = (ClaimsIdentity)principal.Identity;
-			var name_identifer = principal.FindFirstValue(ClaimTypes.NameIdentifier);
-
-			if (!string.IsNullOrEmpty(name_identifer) && user.Age >= MinimumAgeRequirement.MinimumAge)//conditions for InkBallUser to create
-			{
-				InkBallUser found_user = _inkBallContext.InkBallUsers.FirstOrDefault(i => i.sExternalId == name_identifer);
-				if (found_user != null)
-				{
-					//user already created and existing in InkBallUsers, awesome.
-					//just update user name if differs
-					if (found_user.UserName != user.Name)
-					{
-						found_user.UserName = user.Name;
-						await _inkBallContext.SaveChangesAsync(Context.RequestAborted);
-					}
-				}
-				else
-				{
-					found_user = new InkBallUser
-					{
-						sExternalId = name_identifer,
-						iPrivileges = 0,
-						UserName = user.Name
-					};
-					await _inkBallContext.InkBallUsers.AddAsync(found_user, Context.RequestAborted);
-					await _inkBallContext.SaveChangesAsync(Context.RequestAborted);
-				}
-
-				if (!identity.HasClaim(x => x.Type == nameof(InkBall.Module.Pages.HomeModel.InkBallUserId)))
-				{
-					identity.AddClaim(new Claim(nameof(InkBall.Module.Pages.HomeModel.InkBallUserId), found_user.iId.ToString(),
-						nameof(InkBall.Module.Model.InkBallUser)));
-				}
-
-				if (!identity.HasClaim(x => x.Type == ClaimTypes.DateOfBirth))
-				{
-					var date_of_birth = new Claim(ClaimTypes.DateOfBirth,
-						DateTime.UtcNow.AddYears(-user.Age).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
-					identity.AddClaim(date_of_birth);
-				}
-			}
+			await InkBall.Module.Authentication.InkBallCreateUserPrincipalAsync(user, 
+				principal, _inkBallContext, Context.RequestAborted);
 
 			return principal;
 		}
 
 		public override async Task SignOutAsync()
 		{
-			var token = base.Context.RequestAborted;
-			var name_identifer = base.Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var token = Context.RequestAborted;
 
-			var games_to_surrender = await _inkBallContext.InkBallGame
-				.Include(gp1 => gp1.Player1).ThenInclude(p1 => p1.User)
-				.Include(gp2 => gp2.Player2).ThenInclude(p2 => p2.User)
-				.Where(w =>
-				   (w.Player1.User.sExternalId == name_identifer || w.Player2.User.sExternalId == name_identifer)
-				   && (w.GameState == InkBallGame.GameStateEnum.ACTIVE || w.GameState == InkBallGame.GameStateEnum.AWAITING)
-				).ToArrayAsync(token);
-
-			if (games_to_surrender.Any())
-			{
-				using (var trans = await _inkBallContext.Database.BeginTransactionAsync(token))
-				{
-					try
-					{
-						foreach (InkBallGame game in games_to_surrender)
-						{
-							await _inkBallContext.SurrenderGameFromPlayerAsync(game, base.Context.Session, false, token);
-
-							if (_inkballHubContext != null)
-							{
-								InkBallPlayer player_not_signed_off, player_signed_off;
-								if (game.GetPlayer()?.User?.sExternalId == name_identifer)
-								{
-									player_signed_off = game.GetPlayer();
-									player_not_signed_off = game.GetOtherPlayer();
-								}
-								else
-								{
-									player_signed_off = game.GetOtherPlayer();
-									player_not_signed_off = game.GetPlayer();
-								}
-
-								var tsk = Task.Factory.StartNew(async (payload) =>
-								{
-									try
-									{
-										var signedOff_id_online = payload as Tuple<string, int?, string>;
-
-										if (!string.IsNullOrEmpty(signedOff_id_online.Item1))
-										{
-											await _inkballHubContext.Clients.User(signedOff_id_online.Item1).ServerToClientPlayerSurrender(
-												new PlayerSurrenderingCommand(signedOff_id_online.Item2.GetValueOrDefault(0), true,
-												$"Player {signedOff_id_online.Item3 ?? ""} logged out"));
-										}
-									}
-									catch (Exception ex)
-									{
-										Logger.LogError(ex.Message);
-									}
-								},
-								Tuple.Create(player_not_signed_off?.User?.sExternalId, player_signed_off?.iId, player_signed_off?.User?.UserName),
-								token);
-							}
-						}
-
-						trans.Commit();
-					}
-					catch (Exception ex)
-					{
-						trans.Rollback();
-						Logger.LogError(ex, ex.Message);
-					}
-				}
-			}
+			await InkBall.Module.Authentication.InkBallSignOutActionAsync(
+				Context.User.FindFirstValue(ClaimTypes.NameIdentifier),
+				_inkBallContext, _inkballHubContext, Logger, Context.Session, token);
 
 			await base.SignOutAsync();
 		}
