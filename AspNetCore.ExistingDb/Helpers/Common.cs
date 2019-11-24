3e32d0143a6009fee6899b5b144982cf8ecae892
ChaosEngine <andrzej.pauli@gmail.com>
2019-11-24 19:17:52 +0100

    DevReloadOptions in separate class

--- AspNetCore.ExistingDb/Helpers/Common.cs
+++ AspNetCore.ExistingDb/Helpers/Common.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿#if DEBUG
+using Abiosoft.DotNet.DevReload;
+#endif
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
@@ -57,4 +60,37 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 			//DBConfig = "<null>";
 		}
 	}
+
+#if DEBUG
+	public sealed class MyDevReloadOptions : DevReloadOptions
+	{
+		public MyDevReloadOptions()
+		{
+			Directory = "./";
+			IgnoredSubDirectories = new string[] { ".git", ".node_modules", "bin", "obj" };
+			StaticFileExtensions = new string[] { "css", "js", "html", "cshtml" };
+			MaxConnectionFailedCount = 20;
+			CheckIntervalDelay = 2000;
+			PopoutHtmlTemplate = @"<div id='reload' class='toast' role='alert' aria-live='assertive' aria-atomic='true'
+	data-autohide='false' data-animation='true' style='position: absolute; top: 0; right: 0; z-index: 9999'>
+  <div class='toast-header'>
+    <svg class='bd-placeholder-img rounded mr-2' width='20' height='20' xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid slice' focusable='false' role='img'><rect width = '100%' height='100%' fill='red'></rect></svg>
+    <strong class='mr-auto'>DevReload</strong>
+    <small>just now</small>
+    <button type='button' class='ml-2 mb-1 close' data-dismiss='toast' aria-label='Close'>
+      <span aria-hidden='true'>×</span>
+    </button>
+  </div>
+  <div class='toast-body'>
+    DevReload - Reloading page...
+  </div>
+</div>
+<script>
+	$('#reload').toast('hide');
+</script>";
+			TemplateActivationJSFragment = @"$('#reload').toast('show');";
+		}
+	}
+#endif
+
 }
