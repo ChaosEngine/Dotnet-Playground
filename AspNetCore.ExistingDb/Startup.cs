896c96841deb35677cf3bfe5d5a08af3bd230dba
ChaosEngine <andrzej.pauli@gmail.com>
2020-12-04 18:47:23 +0100

    ASPNETCORE_URLS socket optional change, bumps: Microsoft.NET.Test.Sdk, Google.Apis.YouTube.v3

--- AspNetCore.ExistingDb/Startup.cs
+++ AspNetCore.ExistingDb/Startup.cs
@@ -61,7 +61,15 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 			.ConfigureWebHostDefaults(webBuilder =>
 			{
 				webBuilder
-				.UseKestrel(opts => opts.AddServerHeader = false)
+				.UseKestrel(opts =>
+				{
+					opts.AddServerHeader = false;
+					string ASPNETCORE_URLS = Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+					if (!string.IsNullOrEmpty(ASPNETCORE_URLS) && ASPNETCORE_URLS.Contains("unix:/"))
+					{
+						opts.ListenUnixSocket("/sockets/www.sock");
+					}
+				})
 				//.UseLibuv()
 				.UseSockets()
 				/*.UseLinuxTransport(async opts =>
@@ -97,7 +105,15 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 				.ConfigureWebHost(webBuilder =>
 				{
 					webBuilder
-					.UseKestrel(opts => opts.AddServerHeader = false)
+					.UseKestrel(opts =>
+					{
+						opts.AddServerHeader = false;
+						string ASPNETCORE_URLS = Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+						if (!string.IsNullOrEmpty(ASPNETCORE_URLS) && ASPNETCORE_URLS.Contains("unix:/"))
+						{
+							opts.ListenUnixSocket("/sockets/www.sock");
+						}
+					})
 					//.UseLibuv()
 					.UseSockets()
 					.UseLinuxTransport(async opts =>
@@ -283,11 +299,6 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 
 
 
-
-
-
-#region WIP
-
 			services.AddAuthorization(options =>
 			{
 				options.AddPolicy("RequireAdministratorRole",
@@ -310,7 +321,6 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 			});
 
 
-
 			services.AddIdentityManager(options =>
 			{
 				options.SecurityConfiguration.RoleClaimType = "role";
@@ -326,11 +336,6 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 				options.TitleNavBarLinkTarget = Configuration["AppRootPath"];
 			})
 			.AddIdentityMangerService<AspNetCoreIdentityManagerService<ApplicationUser, string, IdentityRole, string>>();
-
-#endregion WIP
-
-
-
 		}
 
 		private void UseProxyForwardingAndDomainPathHelper(IApplicationBuilder app)
