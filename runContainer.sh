95838ac5f4218ed6f9572763a8c7011a46794e2a
ChaosEngine <andrzej.pauli@gmail.com>
2020-12-09 21:21:18 +0100

    some runContainer.sh script trivial changes

--- runContainer.sh
+++ runContainer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 # Build:
-#		DOCKER_BUILDKIT=1 docker build \
-#			--build-arg SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
-#			--build-arg SOURCE_COMMIT=$(git rev-parse HEAD) \
-#			--build-arg PROJECT_VERSION=$(xmllint --xpath 'string(//Version)' AspNetCore.ExistingDb/AspNetCore.ExistingDb.csproj) \
-#			-f Dockerfile.alpine \
-#			-t chaosengine/aspnetcore:alpine3.1 .
+#		DOCKER_BUILDKIT=1 $DOCKER build \
+#		    --build-arg SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+#		    --build-arg SOURCE_COMMIT=$(git rev-parse HEAD) \
+#		    --build-arg PROJECT_VERSION=$(xmllint --xpath 'string(//Version)' AspNetCore.ExistingDb/AspNetCore.ExistingDb.csproj) \
+#		    -f Dockerfile.alpine \
+#		    -t chaosengine/dotnetplayground:alpine5.0 .
 #
 # Run:
 #		docker run -it --rm -p 8080:5000 --name aspnetcore --env-file docker-env.txt -v /home/container/EntityFramework.Docs/sockets:/sockets -v /home/container/EntityFramework.Docs/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery chaosengine/aspnetcore
