eb2feba576163dd6018815060128e8a9057a20e0
ChaosEngine <andrzej.pauli@gmail.com>
2021-02-21 09:42:57 +0100

    paging and filtering

---
+++ DotnetPlayground.Web/GraphQL/Extensions/UseApplicationDbContextAttribute.cs
@@ -0,0 +1,22 @@
+﻿using DotnetPlayground.Models;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DotnetPlayground.GraphQL.Extensions
+{
+    public class UseBloggingContextAttribute : ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(
+            IDescriptorContext context,
+            IObjectFieldDescriptor descriptor,
+            MemberInfo member)
+        {
+            descriptor.UseDbContext<BloggingContext>();
+        }
+    }
+}
---
+++ DotnetPlayground.Web/GraphQL/Query.cs
@@ -0,0 +1,41 @@
+﻿using DotnetPlayground.GraphQL.Extensions;
+using DotnetPlayground.Models;
+using DotnetPlayground.Web.GraphQL.DataLoader;
+using HotChocolate;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetPlayground.GraphQL
+{
+	public class Query
+	{
+		[UseBloggingContext]
+		[UsePaging]
+		[HotChocolate.Data.UseProjection]
+		[HotChocolate.Data.UseFiltering]
+		[HotChocolate.Data.UseSorting]
+		public IQueryable<Blog> GetBlogs([ScopedService] BloggingContext db)
+		{
+			return db.Blogs;
+		}
+
+		public Task<Blog> GetBlogAsync(int id, BlogByIdDataLoader dataLoader, CancellationToken cancellationToken) =>
+			dataLoader.LoadAsync(id, cancellationToken);
+
+
+
+		[UseBloggingContext]
+		[UsePaging(MaxPageSize = 10000)]
+		[HotChocolate.Data.UseProjection]
+		[HotChocolate.Data.UseFiltering]
+		[HotChocolate.Data.UseSorting]
+		public IQueryable<ThinHashes> GetHashes([ScopedService] BloggingContext db)
+		{
+			return db.ThinHashes;
+		}
+	}
+}
