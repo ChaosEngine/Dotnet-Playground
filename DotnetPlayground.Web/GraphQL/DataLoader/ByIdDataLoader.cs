58f34af744229ee8697fc87637b178157d38bc09
ChaosEngine <andrzej.pauli@gmail.com>
2021-02-21 09:42:57 +0100

    paging and filtering

---
+++ DotnetPlayground.Web/GraphQL/DataLoader/ByIdDataLoader.cs
@@ -0,0 +1,37 @@
+﻿using DotnetPlayground.Models;
+using GreenDonut;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetPlayground.Web.GraphQL.DataLoader
+{
+	public class BlogByIdDataLoader : BatchDataLoader<int, Blog>
+	{
+		private readonly IDbContextFactory<BloggingContext> _dbContextFactory;
+
+		public BlogByIdDataLoader(
+			IBatchScheduler batchScheduler,
+			IDbContextFactory<BloggingContext> dbContextFactory)
+			: base(batchScheduler)
+		{
+			_dbContextFactory = dbContextFactory ??
+				throw new ArgumentNullException(nameof(dbContextFactory));
+		}
+
+		protected override async Task<IReadOnlyDictionary<int, Blog>> LoadBatchAsync(
+			IReadOnlyList<int> keys,
+			CancellationToken cancellationToken)
+		{
+			await using BloggingContext dbContext =
+				_dbContextFactory.CreateDbContext();
+
+			return await dbContext.Blogs
+				.Where(s => keys.Contains(s.BlogId))
+				.ToDictionaryAsync(t => t.BlogId, cancellationToken);
+		}
+	}
+}
