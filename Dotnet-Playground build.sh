e57a8babec028c53e0cda35c0b3639a23309466c
ChaosEngine <andrzej.pauli@gmail.com>
2021-02-28 11:27:31 +0100

    only one Dockerfile per image with conditional build for Release and oracle configurations inside added build shell script

---
+++ Dotnet-Playground build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DOCKER=/usr/bin/docker
+#DOCKER=echo
+pushd /home/container/Dotnet-Playground
+image=''
+dockerfile_name=''
+dockerfile_suffix=''
+dockerfile_args=''
+
+case $1 in
+    "latest"|"ubuntu"|"default"|"debian")
+				image="latest";
+				dockerfile_name="Dockerfile";
+				echo "image would be $image";
+			;;
+    "" | "alpine")
+				image="alpine";
+				dockerfile_name="Dockerfile.alpine";
+				echo "image would be $image";
+			;;
+esac
+
+case $2 in
+    "oracle")
+				dockerfile_suffix=".oracle"
+				dockerfile_args="--build-arg BUILD_CONFIG=Oracle"
+				echo "subimage would be $dockerfile_suffix"
+			;;
+    *)
+				dockerfile_suffix=""
+				dockerfile_args=""
+				echo "default subimage"
+			;;
+esac
+
+#echo "${dockerfile_name}${dockerfile_suffix} | chaosengine/dotnetplayground:${image}${dockerfile_suffix}_last"
+#exit 0
+
+$DOCKER tag "chaosengine/dotnetplayground:${image}${dockerfile_suffix}" "chaosengine/dotnetplayground:${image}${dockerfile_suffix}_last"
+
+DOCKER_BUILDKIT=1 $DOCKER build \
+	--build-arg SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+	--build-arg SOURCE_COMMIT=$(git rev-parse HEAD) $dockerfile_args \
+	--progress=auto \
+	-f "${dockerfile_name}" \
+	-t "chaosengine/dotnetplayground:${image}${dockerfile_suffix}" .
+
+popd
+
