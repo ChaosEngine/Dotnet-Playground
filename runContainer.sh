74aec606f56f5f6c69bd6c697f2ff009a5efa0f9
ChaosEngine <andrzej.pauli@gmail.com>
2021-11-13 22:23:21 +0100

    docker build image changes

--- runContainer.sh
+++ runContainer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # Build:
-#		DOCKER_BUILDKIT=1 $DOCKER build \
+#		DOCKER_BUILDKIT=1 docker build \
 #		    --build-arg SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
 #		    --build-arg SOURCE_COMMIT=$(git rev-parse HEAD) \
 #		    -f Dockerfile.alpine \
-#		    -t chaosengine/dotnetplayground:alpine5.0 .
+#		    -t chaosengine/dotnetplayground:alpine6.0 .
 #
 # Run:
-#		docker run -it --rm -p 8080:5000 --name dotnetplayground --env-file docker-env.txt -v /home/container/Dotnet-Playground/sockets:/sockets -v /home/container/Dotnet-Playground/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery chaosengine/dotnetplayground
-#		docker run -it --rm -p 8080:5000 --name dotnetplayground --env-file docker-env.txt -v /home/container/Dotnet-Playground/sockets:/sockets -v /home/container/Dotnet-Playground/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery chaosengine/dotnetplayground:alpine
+#		docker run -it --rm -p 8080:5000 --name dotnetplayground --env-file docker-env.txt -v /home/container/Dotnet-Playground/sockets:/sockets -v /home/container/Dotnet-Playground/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery chaosengine/dotnetplayground:6.0
+#		docker run -it --rm -p 8080:5000 --name dotnetplayground --env-file docker-env.txt -v /home/container/Dotnet-Playground/sockets:/sockets -v /home/container/Dotnet-Playground/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery chaosengine/dotnetplayground:alpine6.0
 #
-docker run -d --rm -p 8080:5000 --name dotnetplayground --hostname DotnetPlayground --env-file docker-prod.env -v /run/mysqld:/sockets -v $(pwd)/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery:ro chaosengine/dotnetplayground:alpine
+docker run -d --rm -p 8080:5000 --name dotnetplayground --hostname DotnetPlayground --env-file docker-prod.env -v /run/mysqld:/sockets -v $(pwd)/shared:/shared -v /var/www/localhost/htdocs/webcamgallery:/webcamgallery:ro chaosengine/dotnetplayground:alpine6.0
