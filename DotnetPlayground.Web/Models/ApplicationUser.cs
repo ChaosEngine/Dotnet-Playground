2f3f46c77a62ffa61c1804d56ec999177ed326e5
ChaosEngine <andrzej.pauli@gmail.com>
2021-11-13 17:48:06 +0100

    JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull }

--- DotnetPlayground.Web/Models/ApplicationUser.cs
+++ DotnetPlayground.Web/Models/ApplicationUser.cs
@@ -2,13 +2,14 @@ using InkBall.Module.Model;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DotnetPlayground.Models
 {
 	// Add profile data for application users by adding properties to the ApplicationUser class
 	public class ApplicationUser : IdentityUser, INamedAgedUser
 	{
-		static readonly JsonSerializerOptions _serializationOpts = new JsonSerializerOptions { IgnoreNullValues = true };
+		static readonly JsonSerializerOptions _serializationOpts = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
 
 		[ProtectedPersonalData]
 		public string Name { get; set; }
