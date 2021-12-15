71862ae101ac5685f4873ea5447476755bd7092a
ChaosEngine <andrzej.pauli@gmail.com>
2021-12-15 21:06:11 +0100

    fixing up dispose error, bump of HotChcolate version

--- DotnetPlayground.Web/GraphQL/Extensions/UseApplicationDbContextAttribute.cs
+++ DotnetPlayground.Web/GraphQL/Extensions/UseApplicationDbContextAttribute.cs
@@ -1,22 +1,29 @@
 ﻿using DotnetPlayground.Models;
+using HotChocolate.Data;
 using HotChocolate.Types;
 using HotChocolate.Types.Descriptors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace DotnetPlayground.GraphQL.Extensions
 {
-    public class UseBloggingContextAttribute : ObjectFieldDescriptorAttribute
-    {
-        public override void OnConfigure(
-            IDescriptorContext context,
-            IObjectFieldDescriptor descriptor,
-            MemberInfo member)
-        {
-            descriptor.UseDbContext<BloggingContext>();
-        }
-    }
+	public class UseBloggingContextAttribute : ObjectFieldDescriptorAttribute
+	{
+		public UseBloggingContextAttribute([CallerLineNumber] int order = 0)
+		{
+			Order = order;
+		}
+
+		public override void OnConfigure(
+			IDescriptorContext context,
+			IObjectFieldDescriptor descriptor,
+			MemberInfo member)
+		{
+			descriptor.UseDbContext<BloggingContext>();
+		}
+	}
 }
