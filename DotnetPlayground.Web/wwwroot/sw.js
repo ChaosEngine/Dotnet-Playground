419914fe96877de20843c515e8d7f9a74b511112
ChaosEngine <andrzej.pauli@gmail.com>
2022-01-07 11:12:25 +0100

    replacing faker.js (dead) with chance

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -65,7 +65,6 @@ self.addEventListener('install', function (event) {
 			'lib/forge/forge.min.js',
 			'lib/bootstrap-table/bootstrap-table.min.css',
 			'lib/bootstrap-table/bootstrap-table.min.js',
-			//'lib/faker/faker.min.js',//questionable, big script ?
 			'lib/jquery-validation/jquery.validate.min.js',
 			'lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js',
 			'lib/ace-builds/ace.js',//questionable, only dev ?
@@ -85,7 +84,7 @@ self.addEventListener('install', function (event) {
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/js/blueimp-gallery.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/video.js/7.15.4/alt/video.core.novtt.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.19.1/bootstrap-table.min.js',
-			//'https://cdn.jsdelivr.net/npm/faker@5.5.3/dist/faker.min.js',//questionable, big script ?
+			//'https://cdn.jsdelivr.net/npm/chance@1.1.8/dist/chance.min.js',
 			'https://cdn.jsdelivr.net/npm/node-forge@1.1.0/dist/forge.min.js',
 			'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
 			'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
