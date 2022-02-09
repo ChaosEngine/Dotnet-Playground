fb8ec036750c852371f3f39970191b79911375df
ChaosEngine <andrzej.pauli@gmail.com>
2022-02-09 20:13:15 +0100

    adding needed security and others stable clientside libraries

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -79,11 +79,11 @@ self.addEventListener('install', function (event) {
 		//cdn resources
 		RESOURCES = RESOURCES.concat([
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/css/blueimp-gallery.min.css',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/7.15.4/video-js.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/7.17.3/video-js.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.19.1/bootstrap-table.min.css',
 			'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/js/blueimp-gallery.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/7.15.4/alt/video.core.novtt.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/7.17.3/alt/video.core.novtt.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.19.1/bootstrap-table.min.js',
 			//'https://cdn.jsdelivr.net/npm/chance@1.1.8/dist/chance.min.js',
 			'https://cdn.jsdelivr.net/npm/node-forge@1.2.1/dist/forge.min.js',
