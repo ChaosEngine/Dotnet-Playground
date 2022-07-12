8a4c7b9dad654bffa1f66940f00f09d311c6ba8f
ChaosEngine <andrzej.pauli@gmail.com>
2022-07-12 19:27:19 +0200

    .NET 6.0.7 and .NET SDK 6.0.107

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -93,9 +93,9 @@ self.addEventListener('install', function (event) {
 			'https://cdn.jsdelivr.net/npm/jquery-validation-unobtrusive@3.2.12/dist/jquery.validate.unobtrusive.min.js'
 			//'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js',//questionable, only dev ?
 			//'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-csharp.min.js',//questionable, only dev ?
-			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr@6.0.6/dist/browser/signalr.min.js',//questionable, inkball ?
+			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr@6.0.7/dist/browser/signalr.min.js',//questionable, inkball ?
 			//'https://cdn.jsdelivr.net/npm/msgpack5@6.0.1/dist/msgpack5.min.js',//questionable, inkball ?
-			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr-protocol-msgpack@6.0.6/dist/browser/signalr-protocol-msgpack.min.js'//questionable, inkball ?
+			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr-protocol-msgpack@6.0.7/dist/browser/signalr-protocol-msgpack.min.js'//questionable, inkball ?
 		]);
 	}
 
