40f3d2a5723fd708b987cca19ba4a47b4d1afa4c
ChaosEngine <andrzej.pauli@gmail.com>
2022-07-20 21:04:39 +0200

    bump: bootstrap-5.2.0, ace-builds, jquery-validation-unobtrusive

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -90,7 +90,7 @@ self.addEventListener('install', function (event) {
 			'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
 			'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js',
-			'https://cdn.jsdelivr.net/npm/jquery-validation-unobtrusive@3.2.12/dist/jquery.validate.unobtrusive.min.js'
+			'https://cdn.jsdelivr.net/npm/jquery-validation-unobtrusive@4.0.0/dist/jquery.validate.unobtrusive.min.js'
 			//'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js',//questionable, only dev ?
 			//'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-csharp.min.js',//questionable, only dev ?
 			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr@6.0.7/dist/browser/signalr.min.js',//questionable, inkball ?
