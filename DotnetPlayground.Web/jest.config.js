e83abd26a1a143d68ec46856c415791dc4c8e575
ChaosEngine <andrzej.pauli@gmail.com>
2022-08-13 14:04:26 +0200

    working jest testing framework

--- DotnetPlayground.Web/jest.config.js
+++ DotnetPlayground.Web/jest.config.js
@@ -1,3 +1,4 @@
+/*global module*/
 /*
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
@@ -17,7 +18,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
@@ -121,12 +122,14 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: '../InkBall/test/InkBall.Tests/clients.jest.tests/',
+  //rootDir: '../InkBall/test/InkBall.Tests/clients.jest.tests/',
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    // "<rootDir>",
+    "../DotnetPlayground.Tests/client.jest.tests/",
+    "../InkBall/test/InkBall.Tests/client.jest.tests/"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -134,7 +137,7 @@ module.exports = {
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
 
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  // A list of paths to modules that run some code toconfigure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
@@ -145,6 +148,7 @@ module.exports = {
 
   // The test environment that will be used for testing
   // testEnvironment: "jest-environment-node",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
