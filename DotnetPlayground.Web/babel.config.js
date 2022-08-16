a075137c308b9258d238d79a22d19f0b58923819
ChaosEngine <andrzej.pauli@gmail.com>
2022-08-16 10:14:04 +0200

    jest tests for inkball game construct  is somewhat ...working

--- DotnetPlayground.Web/babel.config.js
+++ DotnetPlayground.Web/babel.config.js
@@ -1,4 +1,5 @@
 /*global module*/
 module.exports = {
-	presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+	presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+	plugins: ['@babel/transform-runtime', 'babel-plugin-transform-import-meta']
 };
