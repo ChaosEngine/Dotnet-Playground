faf5a06a8e65df9c23011e98e0a4e472cce83a82
ChaosEngine <andrzej.pauli@gmail.com>
2022-09-27 14:36:02 +0200

    playwright: game creation test

--- DotnetPlayground.Web/e2e/TwoUsers.spec.js
+++ DotnetPlayground.Web/e2e/TwoUsers.spec.js
@@ -21,11 +21,54 @@ async function testLoggedInAndNoGameAllert(page, userName) {
 	// await page.screenshot({ path: `./e2e/screenshot-${userName}.png` });
 }
 
-// Use adminPage and userPage fixtures in the test.
+async function testLoggedInGamesList(page) {
+	await page.goto('InkBall/GamesList');
+
+	const legend = page.locator('text=New game creation');
+	await expect(legend).toBeVisible();
+
+	const btnNewGame = page.locator('input[type=submit]', { hasText: 'New game' });
+	await expect(btnNewGame).toHaveClass("btn btn-primary");
+	await expect(btnNewGame).toBeVisible();
+}
+
 test('Playwright0 and Playwright1 - no games created', async ({ Playwright0, Playwright1, Anonymous }) => {
-	// ... interact with both Playwright0 and Playwright1 ...
+	// ... interact with Playwright0 and/or Playwright1 ...
 
 	await testLoggedInAndNoGameAllert(Playwright0.page, Playwright0.userName);
 
 	await testLoggedInAndNoGameAllert(Playwright1.page, Playwright1.userName);
+});
+
+test('Playwright0 and Playwright1 - GamesList', async ({ Playwright0, Playwright1 }) => {
+	// ... interact with Playwright0 and/or Playwright1 ...
+
+	await testLoggedInGamesList(Playwright0.page);
+});
+
+test('P0 create game, P1 joins', async ({ Playwright0, Playwright1 }) => {
+	// ... interact with Playwright0 and/or Playwright1 ...
+
+	await Playwright0.page.goto('InkBall/Home');
+
+	const p0_GameType = Playwright0.page.locator('select#GameType');
+	p0_GameType.selectOption({ label: 'First capture wins' });
+
+	const p0_BoardSize = Playwright0.page.locator('select#BoardSize');
+	p0_BoardSize.selectOption({ label: '20 x 26' });
+
+	const p0_btnNewGame = Playwright0.page.locator('input[type=submit]', { hasText: 'New game' });
+	await expect(p0_btnNewGame).toBeVisible();
+	await p0_btnNewGame.click();
+
+
+	await Playwright1.page.goto('InkBall/GamesList');
+
+	const p1_Join = Playwright1.page.locator('input[type=submit]', { hasText: 'Join' });
+	await expect(p1_Join).toBeVisible();
+	await (p1_Join).click();
+
+	const p0_btnCancel = Playwright0.page.locator('input[type=submit]#SurrenderButton');
+	await expect(p0_btnCancel).toBeVisible();
+	await p0_btnCancel.click();
 });
