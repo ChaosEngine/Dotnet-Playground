fdf6a379d1c057bbeacc804dd9407f0d4dd5a960
ChaosEngine <andrzej.pauli@gmail.com>
2022-09-27 14:34:48 +0200

    playwright: anonymous refactoring

--- DotnetPlayground.Web/e2e/Anonymous.spec.js
+++ DotnetPlayground.Web/e2e/Anonymous.spec.js
@@ -6,7 +6,7 @@ import { test, expect } from '@playwright/test';
  * @param {object} browser playwright
  * @param {string} pageUrl url to page
  */
-async function notAllowedAndRedirecttoLogin(browser, pageUrl) {
+async function notAllowedAndRedirectToLogin(browser, pageUrl) {
 	const page = await browser.newPage();
 
 	page.goto(pageUrl);
@@ -24,40 +24,38 @@ async function notAllowedAndRedirecttoLogin(browser, pageUrl) {
 
 test.describe.configure({ mode: 'parallel' });
 
-test.describe('Logged in as Anonymous', () => {
-	test.use({ storageState: './e2e/storageStates/Anonymous-storageState.json', ignoreHTTPSErrors: true });
+test.use({ storageState: './e2e/storageStates/Anonymous-storageState.json', ignoreHTTPSErrors: true });
 
-	test('Home as Anonymous', async ({ browser }) => {
-		const page = await browser.newPage();
+test('Home as Anonymous', async ({ browser }) => {
+	const page = await browser.newPage();
 
-		page.goto('InkBall/Home');
+	page.goto('InkBall/Home');
 
-		// Expect a title "to contain" a substring.
-		await expect(page).toHaveTitle(/Home - Dotnet Core Playground/);
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Home - Dotnet Core Playground/);
 
-		//Not logged-in message
-		const welcome = page.locator('p.inkhome');
-		await expect(welcome).toContainText(/You are not logged in ... or allowed 😅/);
-	});
+	//Not logged-in message
+	const welcome = page.locator('p.inkhome');
+	await expect(welcome).toContainText(/You are not logged in ... or allowed 😅/);
+});
 
-	test('Game page as Anonymous with redirect to LogIn', async ({ browser }) => {
-		await notAllowedAndRedirecttoLogin(browser, 'InkBall/Game');
-	});
+test('Game page as Anonymous with redirect to LogIn', async ({ browser }) => {
+	await notAllowedAndRedirectToLogin(browser, 'InkBall/Game');
+});
 
-	test('GamesList page as Anonymous with redirect to LogIn', async ({ browser }) => {
-		await notAllowedAndRedirecttoLogin(browser, 'InkBall/GamesList');
-	});
+test('GamesList page as Anonymous with redirect to LogIn', async ({ browser }) => {
+	await notAllowedAndRedirectToLogin(browser, 'InkBall/GamesList');
+});
 
-	test('Highscores page as Anonymous with redirect to LogIn', async ({ browser }) => {
-		await notAllowedAndRedirecttoLogin(browser, 'InkBall/Highscores');
-	});
+test('Highscores page as Anonymous with redirect to LogIn', async ({ browser }) => {
+	await notAllowedAndRedirectToLogin(browser, 'InkBall/Highscores');
+});
 
-	test('NotExisting page as Anonymous with redirect to LogIn', async ({ browser }) => {
-		const page = await browser.newPage();
+test('NotExisting page as Anonymous with redirect to LogIn', async ({ browser }) => {
+	const page = await browser.newPage();
 
-		const resp = await page.goto('InkBall/NotExisting');
+	const resp = await page.goto('InkBall/NotExisting');
 
-		// Expects the URL to be Home because Game page redirect if no game present.
-		await expect(resp?.status()).toBe(404);
-	});
+	// Expects the URL to be Home because Game page redirect if no game present.
+	await expect(resp?.status()).toBe(404);
 });
