d5004cafe157b4fc253ae15c90752cf3933cc112
ChaosEngine <andrzej.pauli@gmail.com>
2022-10-12 23:31:20 +0200

    preparing CircleCI for playwirght tests

--- DotnetPlayground.Web/playwright.config.js
+++ DotnetPlayground.Web/playwright.config.js
@@ -97,16 +97,18 @@ const config = {
 		//     channel: 'chrome',
 		//   },
 		// },
-	]
+	],
 
 	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
 	// outputDir: 'test-results/',
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   port: 3000,
-	// },
+	webServer: {
+		command: 'dotnet run',
+		url: 'https://localhost:4553/dotnet/',
+		timeout: 120 * 1000,
+		ignoreHTTPSErrors: true
+	}
 };
 
 export default config;
