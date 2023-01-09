9816b23a0c226554b96a719055ac270de64594ce
ChaosEngine <andrzej.pauli@gmail.com>
2023-01-09 22:22:49 +0100

    circle-ci split tests into multiple jobs

--- DotnetPlayground.Web/playwright.config.js
+++ DotnetPlayground.Web/playwright.config.js
@@ -32,7 +32,7 @@ const config = {
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	workers: process.env.CI ? undefined : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [['html', { outputFolder: 'e2e/playwright-report' }]],
 	globalSetup: './e2e/global-setup',
