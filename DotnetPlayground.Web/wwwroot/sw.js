2643ee1934d79a051eba54f20a64066773148fb7
ChaosEngine <andrzej.pauli@gmail.com>
2023-08-17 22:42:37 +0200

    bump: sass, video.js, playwright, uptaking other bumps from depndabot

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -87,11 +87,11 @@ self.addEventListener('install', function (event) {
 		//cdn resources
 		RESOURCES = RESOURCES.concat([
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/css/blueimp-gallery.min.css',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.3.0/video-js.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.5.2/video-js.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.1/bootstrap-table.min.css',
 			'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/js/blueimp-gallery.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.3.0/alt/video.core.novtt.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.5.2/alt/video.core.novtt.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.1/bootstrap-table.min.js',
 			//'https://cdn.jsdelivr.net/npm/chance@1.1.11/dist/chance.min.js',
 			'https://cdn.jsdelivr.net/npm/node-forge@1.3.1/dist/forge.min.js',
