ChaosEngine/IdentityManager2

trim compatible serialization code

src/IdentityManager2/Api/Models/JsonSerializers.cs
@@ -1,0 +1,75 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using IdentityManager2.Api.Models;
+using IdentityManager2.Core;
+using IdentityManager2.Core.Metadata;
+
+[JsonSerializable(typeof(PageModelParams))]
+partial class PageModelParams_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(IEnumerable<PropertyMetadata>))]
+[JsonSerializable(typeof(IEnumerable<PropertyDataType>))]
+[JsonSerializable(typeof(PropertyDataType))]
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(AnonymousSubject))]
+[JsonSerializable(typeof(AnonymousSubjectRole))]
+[JsonSerializable(typeof(AnonymousUserName))]
+[JsonSerializable(typeof(AnonymousDetail))]
+[JsonSerializable(typeof(UserDetailResource))]
+[JsonSerializable(typeof(UserDetailDataResource))]
+[JsonSerializable(typeof(UserQueryResultResource))]
+[JsonSerializable(typeof(UserQueryResultResourceData))]
+[JsonSerializable(typeof(MetaResult))]
+public partial class MetaResult_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(UserQueryResultResource))]
+public partial class UserQueryResultResource_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(UserDetailDataResource))]
+[JsonSerializable(typeof(IEnumerable<AnonymousPropertiesDataMetaLink>))]
+[JsonSerializable(typeof(AnonymousUpdate))]
+[JsonSerializable(typeof(AnonymousClaim))]
+[JsonSerializable(typeof(AnonymousRolesDataMetaLink[]))]
+[JsonSerializable(typeof(AnonymousRolesActionLinks))]
+[JsonSerializable(typeof(UserDetailResource))]
+public partial class UserDetailResource_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(RoleQueryResultResource))]
+public partial class RoleQueryResultResource_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(RoleDetailResource))]
+public partial class RoleDetailResource_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(PropertyValue[]))]
+public partial class ArrayPropertyValue_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(ClaimValue))]
+public partial class ClaimValue_Context: JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(AnonymousCreatedRole))]
+public partial class AnonymousCreatedRole_Context : JsonSerializerContext { }
+
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(AnonymousCreatedUser))]
+public partial class AnonymousCreatedUser_Context : JsonSerializerContext { }

ChaosEngine/Dotnet-Playground

DotnetPlayground.Web/Helpers/JsonSerializers.cs
@@ -5,19 +5,78 @@ using System.Text.Json.Serialization;
 
 namespace DotnetPlayground.Web.Helpers
 {
-	[JsonSerializable(typeof(Dictionary<string, string>))]
-	public partial class DictStringString_Context : JsonSerializerContext
-	{
-	}
+    /// Home Page
 
 
-	[JsonSerializable(typeof(DateTime))]
-	public partial class DateTime_Context : JsonSerializerContext
-	{
-	}
+    [JsonSerializable(typeof(Dictionary<string, string>))]
+    public partial class DictStringString_Context : JsonSerializerContext
+    {
+    }
 
-	[JsonSerializable(typeof(RandomData))]
-	public partial class RandomData_Context : JsonSerializerContext
-	{
-	}
+
+
+    [JsonSerializable(typeof(DateTime))]
+    public partial class DateTime_Context : JsonSerializerContext
+    {
+    }
+
+
+    [JsonSerializable(typeof(RandomData))]
+    public partial class RandomData_Context : JsonSerializerContext
+    {
+    }
+
+
+    /// Blogs
+
+    [JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonSerializable(typeof(List<Post>))]
+    public partial class ListPost_Context : JsonSerializerContext { }
+
+    [JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonSerializable(typeof(Post))]
+    public partial class Post_Context : JsonSerializerContext { }
+
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSerializable(typeof(Blog))]
+    public partial class Blog_Context : JsonSerializerContext { }
+
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSerializable(typeof(DecoratedBlog))]
+    public partial class DecoratedBlog_Context : JsonSerializerContext { }
+
+    [JsonSerializable(typeof(string))]
+    public partial class String_Context : JsonSerializerContext { }
+
+    /// CSP
+
+    [JsonSerializable(typeof(CspReportRequest))]
+    public partial class CspReportRequest_Context : JsonSerializerContext { }
+
+
+    /// Hashes
+
+    [JsonSerializable(typeof(HashInput))]
+    public partial class HashInput_Context : JsonSerializerContext { }
+
+    [JsonSerializable(typeof(Hashes))]
+    public partial class Hashes_Context : JsonSerializerContext { }
+
+    [JsonSerializable(typeof(ThinHashes))]
+    public partial class ThinHashes_Context : JsonSerializerContext { }
+
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSerializable(typeof(List<ThinHashes>))]
+    public partial class IEnumerableThinHashes_Context : JsonSerializerContext { }
+
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSerializable(typeof(HashesLoadResult))]
+    public partial class HashesLoadResult_Context : JsonSerializerContext { }
+
+
+    // WebcamGallery
+
+    [JsonSerializable(typeof(AnnualTimelapseBag))]
+    public partial class AnnualTimelapseBag_Context : JsonSerializerContext { }
+
 }