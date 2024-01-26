2938defcf275a5be68a04fe1452b773b30987548
ChaosEngine <andrzej.pauli@gmail.com>
2024-01-26 15:16:42 +0100

    bumps: webpack, video.js

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -89,11 +89,11 @@ self.addEventListener('install', function (event) {
 		//cdn resources
 		RESOURCES = RESOURCES.concat([
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/css/blueimp-gallery.min.css',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.9.0/video-js.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.11.0/video-js.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.2/bootstrap-table.min.css',
 			'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/js/blueimp-gallery.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.9.0/alt/video.core.novtt.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.11.0/alt/video.core.novtt.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.2/bootstrap-table.min.js',
 			//'https://cdn.jsdelivr.net/npm/chance@1.1.11/dist/chance.min.js',
 			'https://cdn.jsdelivr.net/npm/node-forge@1.3.1/dist/forge.min.js',
