8f353b6d9bc97b87d58926351d4ae00c24927f61
ChaosEngine <andrzej.pauli@gmail.com>
2024-07-09 22:11:31 +0200

    bump dependencies: - @microsoft/signalr 8.0.0 + @microsoft/signalr 8.0.7 - @microsoft/signalr-protocol-msgpack 8.0.0 + @microsoft/signalr-protocol-msgpack 8.0.7 - chance 1.1.11 + chance 1.1.12 - video.js 8.12.0 + video.js 8.16.1

--- DotnetPlayground.Web/wwwroot/sw.js
+++ DotnetPlayground.Web/wwwroot/sw.js
@@ -89,11 +89,11 @@ self.addEventListener('install', function (/* event */) {
 		//cdn resources
 		RESOURCES = RESOURCES.concat([
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/css/blueimp-gallery.min.css',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.12.0/video-js.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.16.1/video-js.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.23.0/bootstrap-table.min.css',
 			'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
 			'https://cdnjs.cloudflare.com/ajax/libs/blueimp-gallery/3.4.0/js/blueimp-gallery.min.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.12.0/alt/video.core.novtt.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/video.js/8.16.1/alt/video.core.novtt.min.js',
 			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.23.0/bootstrap-table.min.js',
 			//'https://cdn.jsdelivr.net/npm/chance@1.1.11/dist/chance.min.js',
 			'https://cdn.jsdelivr.net/npm/node-forge@1.3.1/dist/forge.min.js',
@@ -103,9 +103,9 @@ self.addEventListener('install', function (/* event */) {
 			'https://cdn.jsdelivr.net/npm/jquery-validation-unobtrusive@4.0.0/dist/jquery.validate.unobtrusive.min.js'
 			//'https://cdnjs.cloudflare.com/ajax/libs/ace/1.35.2/ace.js',//questionable, only dev ?
 			//'https://cdnjs.cloudflare.com/ajax/libs/ace/1.35.2/mode-csharp.min.js',//questionable, only dev ?
-			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr@7.0.10/dist/browser/signalr.min.js',//questionable, inkball ?
+			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr@8.0.7/dist/browser/signalr.min.js',//questionable, inkball ?
 			//'https://cdn.jsdelivr.net/npm/msgpack5@6.0.2/dist/msgpack5.min.js',//questionable, inkball ?
-			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr-protocol-msgpack@7.0.10/dist/browser/signalr-protocol-msgpack.min.js'//questionable, inkball ?
+			//'https://cdn.jsdelivr.net/npm/@microsoft/signalr-protocol-msgpack@8.0.7/dist/browser/signalr-protocol-msgpack.min.js'//questionable, inkball ?
 		]);
 	}
 
