715c67a353c4605f207843c2aaf688eecccc1b91
ChaosEngine <andrzej.pauli@gmail.com>
2025-03-15 20:20:13 +0100

    refactor: clean up eslint configuration for improved readability and organization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,59 +7,60 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.basename(path.dirname(__filename));
-// console.log("ch__dirname = ", __dirname);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
 });
 
-
 export default [
-    {
-        ignores: ["**/bin", "**/obj", "**/wwwroot/lib", "**/*.min.js", "**/*Bundle.js"]
-    },
-    ...compat.extends("eslint:recommended"), jsdoc.configs['flat/recommended'],
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.jquery,
-                ...globals.worker
-            },
+	// Ignore specific directories and files
+	{
+		ignores: ["**/bin", "**/obj", "**/wwwroot/lib", "**/*.min.js", "**/*Bundle.js"]
+	},
+	// Extend recommended configurations
+	...compat.extends("eslint:recommended"), 
+	jsdoc.configs['flat/recommended'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.jquery,
+				...globals.worker
+			},
+			ecmaVersion: 2022,
+			sourceType: "module"
+		},
+		plugins: {
+			jsdoc
+		},
 
-            ecmaVersion: 2022,
-            sourceType: "module"
-        },
-        plugins: {
-            jsdoc
-        },
 
+		rules: {
+			indent: ["off", "tab"],
+			"linebreak-style": ["off", "unix"],
+			quotes: ["off", "double"],
+			semi: ["error", "always"],
+			eqeqeq: "error",
+			"comma-dangle": "warn",
+			"no-console": "warn",
+			"no-debugger": "warn",
+			"no-extra-semi": "warn",
+			"no-extra-parens": "off",
+			"no-irregular-whitespace": "warn",
+			"no-undef": "error",
+			"no-unused-vars": "warn",
+			"semi-spacing": "warn",
 
-        rules: {
-            indent: ["off", "tab"],
-            "linebreak-style": ["off", "unix"],
-            quotes: ["off", "double"],
-            semi: ["error", "always"],
-            eqeqeq: "error",
-            "comma-dangle": "warn",
-            "no-console": "warn",
-            "no-debugger": "warn",
-            "no-extra-semi": "warn",
-            "no-extra-parens": "off",
-            "no-irregular-whitespace": "warn",
-            "no-undef": "error",
-            "no-unused-vars": "warn",
-            "semi-spacing": "warn",
-
-            "jsdoc/require-returns": "warn",
-            "jsdoc/require-jsdoc": ["error", {
-                checkConstructors: false,
-                publicOnly: true,
-                require: {
-                    'MethodDefinition': true
-                }
-            }]
-        }
-    }];
+			"jsdoc/require-returns": "warn",
+			"jsdoc/require-jsdoc": ["error", {
+				checkConstructors: false,
+				publicOnly: true,
+				require: {
+					'MethodDefinition': true
+				}
+			}]
+		}
+	}
+];
