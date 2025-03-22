0404b1acd3834fc65c9208128687d834df05f16a
ChaosEngine <andrzej.pauli@gmail.com>
2025-03-22 21:15:03 +0100

    ImportCsv: clientside and server side approaches to count cells in csv

--- DotnetPlayground.Web/Pages/ImportCsv.cshtml.cs
+++ DotnetPlayground.Web/Pages/ImportCsv.cshtml.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,59 @@ namespace DotnetPlayground.Pages
 {
 	public class ImportCsvModel : PageModel
 	{
+		/// <summary>
+		/// Represents the result of a CSV count operation.
+		/// </summary>
+		/// <param name="RowsCount">The number of rows in the CSV file.</param>
+		/// <param name="NonEmptyCellsCount">The number of non-empty cells in the CSV file.</param>
+		private record struct CsvResult(int RowsCount, long NonEmptyCellsCount);
+
+
+		/// <summary>
+		/// Gets or sets the CSV data.
+		/// </summary>
+		/// <value>
+		/// A two-dimensional array of strings representing the CSV data.
+		/// </value>
+		/// <remarks>
+		/// This property is bound from the body of the request and is required.
+		/// </remarks>
+		[BindProperty]
+		[FromBody]
+		[Required]
+		public string[][] CsvData { get; set; }
+
 		public void OnGet()
 		{
+		}
+
+		public IActionResult OnPost()
+		{
+			if (!ModelState.IsValid)
+				return new BadRequestObjectResult(ModelState);
+
+			if (CsvData == null || CsvData.Length == 0)
+				return new BadRequestObjectResult("Error: empty or invalid CSV data.");
+
+			// return new BadRequestObjectResult("Error: OMG!!Fake exception.");
+
+			// Process the CSV data here
+			// long counter = 0;
+			// foreach (string[] row in CsvData)
+			// 	foreach (string cell in row)
+			// 		if (!string.IsNullOrEmpty(cell))
+			// 			counter++;
+
+			long counter =
+			CsvData.Aggregate(0, (row_accumulator, row) =>
+			{
+				return row_accumulator + row.Aggregate(0, (cell_acc, cell) =>
+				{
+					return cell_acc + (string.IsNullOrEmpty(cell) ? 0 : 1);
+				});
+			});
+
+			return new JsonResult(new CsvResult(CsvData.Length, counter));
 		}
 	}
 }
--- DotnetPlayground.Web/wwwroot/js/ImportCsv.js
+++ DotnetPlayground.Web/wwwroot/js/ImportCsv.js
@@ -1,5 +1,17 @@
+/*global myAlert, i18next */
+
 window.addEventListener('load', () => {
 
+	let globalData = null;
+
+	/**
+	 * Convert CSV content to HTML table
+	 * @see https://stackoverflow.com/a/1293163/1248177
+	 * @see https://stackoverflow.com/a/1293163/1248177
+	 * @param {string} csvContent CSV content as a string
+	 * @param {string} delimiter Delimiter character
+	 * @returns {HTMLTableElement} HTML table element
+	 */
 	const csvToTable = (csvContent, delimiter) => {
 		// Remove \r and \n inside double-quoted values
 		const csvData = csvContent.replace(/"(.*?)"/gs, (match) => {
@@ -14,6 +26,8 @@ window.addEventListener('load', () => {
 		let section = null;
 		let rowCounter = 0;
 
+		const rawData = [];
+
 		for (const line of lines) {
 			if (line.length === 0 || line.startsWith('#')) continue; // Skip empty lines
 
@@ -23,18 +37,30 @@ window.addEventListener('load', () => {
 			if (!section) {
 				section = table.createTHead();
 				const row = section.insertRow();
+				rawData[rowCounter] = [];
+
 				for (let j = 0; j < values.length; j++) {
 					const cell = document.createElement('th');
-					cell.textContent = values[j].trim();
+					const textual_content = values[j].trim();
+					cell.textContent = textual_content;
 					row.appendChild(cell);
+
+					rawData[rowCounter][j] = textual_content;
+
 				}
 				section = table.createTBody();//switch to body
+				rowCounter++;
 				continue;//done with header, skip to next iteration
 			}
 			const row = section.insertRow();
+			rawData[rowCounter] = [];
+
 			for (let j = 0; j < values.length; j++) {
 				const cell = row.insertCell();
-				cell.textContent = values[j].trim();
+				const textual_content = values[j].trim();
+				cell.textContent = textual_content;
+
+				rawData[rowCounter][j] = textual_content;
 			}
 			rowCounter++;
 		}
@@ -44,7 +70,7 @@ window.addEventListener('load', () => {
 		section.dataset.i18nOptions = `{ 'count': ${rowCounter} }`;
 		section.textContent = `Total rows: ${rowCounter}`;
 
-		return table;
+		return { table, rawData };
 	};
 
 	document.getElementById('csvFile').addEventListener('change', (event) => {
@@ -54,12 +80,13 @@ window.addEventListener('load', () => {
 			const reader = new FileReader();
 
 			reader.onload = function (e) {
-				const csvData = e.target.result;
+				const csv_file = e.target.result;
 				const delimiter = document.getElementById('delimiter').value;
-				const table = csvToTable(csvData, delimiter || ',');
-				if (table) {
+				const { table: table_element, rawData } = csvToTable(csv_file, delimiter || ',');
+				globalData = rawData;
+				if (table_element) {
 					document.getElementById('tableContainer').innerHTML = ''; // Clear existing content
-					document.getElementById('tableContainer').appendChild(table);
+					document.getElementById('tableContainer').appendChild(table_element);
 
 					if (window.localize)
 						window.localize("#tableContainer");
@@ -67,6 +94,100 @@ window.addEventListener('load', () => {
 			};
 
 			reader.readAsText(file);
+		}
+	});
+
+	/**
+	 * Submit the CSV data to the server
+	 * @param {Array<Array<string>>} rawData 2D array of strings
+	 * @returns {Promise<void>}
+	 */
+	async function submitToServerApproach(rawData) {
+		const payload = JSON.stringify(rawData);
+		// Required for AntiForgeryToken
+		const antiforgeryToken = document.querySelector('input[name="__RequestVerificationToken"]').value;
+		try {
+			const response = await fetch('ImportCsv', {
+				method: 'POST',
+				body: payload,
+				credentials: 'same-origin',
+				cache: 'no-cache',
+				redirect: 'follow',
+				referrerPolicy: 'no-referrer',
+				headers: {
+					'Content-Type': 'application/json',
+					'RequestVerificationToken': antiforgeryToken
+				}
+			});
+			if (!response.ok) {
+				const body = await response.text();
+				if (typeof body === 'string' && body.indexOf('Error') !== -1) {
+					// console.log(body);
+					myAlert(i18next.t('importCsv.undErr'), i18next.t('importCsv.Failed'));
+					return;
+				}
+				else
+					myAlert(i18next.t('importCsv.badResp'), i18next.t('importCsv.Failed'));
+				return;
+			}
+			//assuming json response
+			const json = await response.json();
+
+			myAlert(i18next.t('importCsv.goodResult', { rowsCount: json.rowsCount, nonEmptyCellsCount: json.nonEmptyCellsCount }), i18next.t('importCsv.success'));
+
+		} catch (error) {
+			myAlert(i18next.t('importCsv.exInterc'), i18next.t('importCsv.Failed'));
+			// eslint-disable-next-line no-console
+			console.error('Error:', error);
+		}
+	}
+
+	/**
+	 * Count non empty cells in the CSV data
+	 * @param {Array<Array<string>>} rawData 2D array of strings
+	 * @returns {Promise<void>}
+	 */
+	async function countClientSideApproach(rawData) {
+		// rawData is a 2D array of strings
+		// Each inner array represents a row of the CSV file
+		// Each string represents a cell value
+
+		//iterate over rows and cells of rawData
+		const counter = rawData.reduce((row_accumulator, row) => {
+			return row_accumulator + row.reduce((cell_acc, cell) => {
+				return cell_acc + (cell && cell !== '' ? 1 : 0);
+			}, 0);
+		}, 0);
+
+
+		myAlert(i18next.t('importCsv.goodResult', { rowsCount: rawData.length, nonEmptyCellsCount: counter }), i18next.t('importCsv.success'));
+	}
+
+	document.getElementById('csvForm').addEventListener('submit', async (event) => {
+		event.preventDefault();
+
+		//detect if form is valid
+		const form = event.target;
+		if (!form.checkValidity()) {
+			form.reportValidity();
+			return;
+		}
+		if (!globalData || globalData.length === 0) {
+			myAlert(i18next.t('importCsv.noCsv'), i18next.t('importCsv.Failed'));
+			return;
+		}
+
+		const buttonName = event.submitter ? event.submitter.name : null;
+		switch (buttonName) {
+			case 'btnSubmit':
+				await submitToServerApproach(globalData);
+				break;
+			case 'btnCount':
+				await countClientSideApproach(globalData);
+				break;
+			default:
+				myAlert(i18next.t('importCsv.unknBtn'), i18next.t('importCsv.Failed'));
+				break;
 		}
 	});
 
