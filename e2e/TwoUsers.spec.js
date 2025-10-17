892c7cbbed2a64427695dcd44c92f2b4d30c161e
ChaosEngine <andrzej.pauli@gmail.com>
2025-10-17 17:05:04 +0200

    bump: @playwright/test new AI game tests WIP

--- e2e/TwoUsers.spec.js
+++ e2e/TwoUsers.spec.js
@@ -27,7 +27,7 @@ test('Playwright1 and Playwright2 - GamesList', async () => {
 	// ... interact with Playwright1 and/or Playwright2 ...
 
 	await helper.testLoggedInGamesList(Playwright1.page);
-});	 
+});
 
 test('P1 create game, P2 joins, P2 wins', async () => {
 	const p1 = Playwright1, p2 = Playwright2;
@@ -91,71 +91,122 @@ test('P1 create game, P2 joins, P2 wins', async () => {
 
 });
 
-test('AI game: put 5x4 points and AI surrounds it', async () => {
-	const p1 = Playwright1;
+test.describe('AI tests', () => {
+	// test.describe.configure({ mode: 'parallel' });
 
-	// ... interact as Playwright1 only ... create new AI game
-	await helper.createGameFromHome(p1, 'Advantage of 5 paths wins', '40 x 52', true);
+	test('Put 2x2 points and AI surrounds it', async () => {
+		const p1 = Playwright1;
 
-	await helper.delay(1 * 500);//wait for signalR to settle in (?)
+		// ... interact as Playwright1 only ... create new AI game
+		await helper.createGameFromHome(p1, 'Advantage of 5 paths wins', '40 x 52', true);
 
-	let randX = helper.getRandomInt(0, 6), randY = helper.getRandomInt(0, 8);
-	await expect(p1.page.getByText(`Multi CPU Oponent UserPlayer`)).toBeVisible();
+		await helper.delay(1 * 500);//wait for signalR to settle in (?)
 
-
-
-	const svg = await p1.page.locator('svg#screen');
-
-	//put 4x p1 points and let AI surround it 5 times and win
-	await helper.svgClick(svg, randX + 15, randY + 11);//1st point
-	
-	const firstOponentPoint = await svg.locator('circle[data-status="POINT_FREE_BLUE"]');//2nd point is AI
-	const cy = await firstOponentPoint.getAttribute('cy');
-	if (cy) {
-		const cyInt = parseInt(cy);
-		if (cyInt < 26)
-			randY += 26;
-	}
+		let randX = helper.getRandomInt(0, 6), randY = helper.getRandomInt(0, 8);
+		await expect(p1.page.getByText(`Multi CPU Oponent UserPlayer`)).toBeVisible();
 
 
-	await helper.svgClick(svg, randX + 16, randY + 11);
-	await helper.svgClick(svg, randX + 15, randY + 12);
-	await helper.svgClick(svg, randX + 16, randY + 12);
 
-	await helper.svgClick(svg, randX + 20, randY + 11);
-	await helper.svgClick(svg, randX + 21, randY + 11);
-	await helper.svgClick(svg, randX + 20, randY + 12);
-	await helper.svgClick(svg, randX + 21, randY + 12);
+		const svg = await p1.page.locator('svg#screen');
 
-	await helper.svgClick(svg, randX + 25, randY + 11);
-	await helper.svgClick(svg, randX + 26, randY + 11);
-	await helper.svgClick(svg, randX + 25, randY + 12);
-	await helper.svgClick(svg, randX + 26, randY + 12);
+		//put 4x p1 points and let AI surround it 5 times and win
+		await helper.svgClick(svg, randX + 15, randY + 11);//1st point
 
-	await helper.svgClick(svg, randX + 15, randY + 16);
-	await helper.svgClick(svg, randX + 16, randY + 16);
-	await helper.svgClick(svg, randX + 15, randY + 17);
-	await helper.svgClick(svg, randX + 16, randY + 17);
+		const firstOponentPoint = await svg.locator('circle[data-status="POINT_FREE_BLUE"]');//2nd point is AI
+		const cy = await firstOponentPoint.getAttribute('cy');
+		if (cy) {
+			const cyInt = parseInt(cy);
+			if (cyInt < 26)
+				randY += 26;
+		}
 
-	await helper.svgClick(svg, randX + 20, randY + 16);
-	await helper.svgClick(svg, randX + 21, randY + 16);
-	await helper.svgClick(svg, randX + 20, randY + 17);
-	await helper.svgClick(svg, randX + 21, randY + 17);
 
-	//put dummy 2 spread points just to trigger AI to do its work
-	for (let x = 2; x <= 38; x += 2)
-		await helper.svgClick(svg, x, randY + 1);
-	for (let x = 2; x <= 34; x += 2)
-		await helper.svgClick(svg, x, randY + 3);
+		await helper.svgClick(svg, randX + 16, randY + 11);
+		await helper.svgClick(svg, randX + 15, randY + 12);
+		await helper.svgClick(svg, randX + 16, randY + 12);
 
-	await helper.delay(1 * 500);//wait for signalR to settle in (?)
+		await helper.svgClick(svg, randX + 20, randY + 11);
+		await helper.svgClick(svg, randX + 21, randY + 11);
+		await helper.svgClick(svg, randX + 20, randY + 12);
+		await helper.svgClick(svg, randX + 21, randY + 12);
 
-	const winMessageVisible = await p1.page.locator('div.modal-body', { hasText: 'And the winner is... blue.'}).isVisible();
-	if(!winMessageVisible)
-	{
-		await helper.svgClick(svg, 36, randY + 3);//somehow we are lacking last point, put it just to be sure
-		// await helper.svgClick(svg, 38, randY + 3);//just to be sure
-	}
+		await helper.svgClick(svg, randX + 25, randY + 11);
+		await helper.svgClick(svg, randX + 26, randY + 11);
+		await helper.svgClick(svg, randX + 25, randY + 12);
+		await helper.svgClick(svg, randX + 26, randY + 12);
 
-	await helper.verifyWin(p1, 'And the winner is... blue.');
+		await helper.svgClick(svg, randX + 15, randY + 16);
+		await helper.svgClick(svg, randX + 16, randY + 16);
+		await helper.svgClick(svg, randX + 15, randY + 17);
+		await helper.svgClick(svg, randX + 16, randY + 17);
+
+		await helper.svgClick(svg, randX + 20, randY + 16);
+		await helper.svgClick(svg, randX + 21, randY + 16);
+		await helper.svgClick(svg, randX + 20, randY + 17);
+		await helper.svgClick(svg, randX + 21, randY + 17);
+
+		//put dummy 2 spread points just to trigger AI to do its work
+		for (let x = 2; x <= 38; x += 2)
+			await helper.svgClick(svg, x, randY + 1);
+		for (let x = 2; x <= 34; x += 2)
+			await helper.svgClick(svg, x, randY + 3);
+
+		await helper.delay(1 * 500);//wait for signalR to settle in (?)
+
+		const winMessageVisible = await p1.page.locator('div.modal-body', { hasText: 'And the winner is... blue.' }).isVisible();
+		if (!winMessageVisible) {
+			await helper.svgClick(svg, 36, randY + 3);//somehow we are lacking last point, put it just to be sure
+			// await helper.svgClick(svg, 38, randY + 3);//just to be sure
+		}
+
+		await helper.verifyWin(p1, 'And the winner is... blue.');
+	});
+
+
+	test('Put C-like shape points and AI surrounds it', async () => {
+		const p2 = Playwright2;
+
+		// ... interact as Playwright1 only ... create new AI game
+		await helper.createGameFromHome(p2, 'First capture wins', '40 x 52', true);
+
+		await helper.delay(1 * 500);//wait for signalR to settle in (?)
+
+		let randX = helper.getRandomInt(0, 6), randY = helper.getRandomInt(0, 8);
+		await expect(p2.page.getByText(`Multi CPU Oponent UserPlayer`)).toBeVisible();
+
+
+
+		const svg = await p2.page.locator('svg#screen');
+
+		//put 4x p1 points and let AI surround it 5 times and win
+		await helper.svgClick(svg, randX + 10, randY + 5);//1st point
+
+		const firstOponentPoint = await svg.locator('circle[data-status="POINT_FREE_BLUE"]');//2nd point is AI
+		const cy = await firstOponentPoint.getAttribute('cy');
+		if (cy) {
+			const cyInt = parseInt(cy);
+			if (cyInt < 26)
+				randY += 26;
+		}
+
+		for (let x = 10; x >= 5; x--)
+			await helper.svgClick(svg, randX + x, randY + 5);
+		for (let y = 5; y <= 10; y++)
+			await helper.svgClick(svg, randX + 5, randY + y);
+		for (let x = 5; x <= 10; x++)
+			await helper.svgClick(svg, randX + x, randY + 10);
+
+		//put dummy 2 spread points just to trigger AI to do its work
+		for (let x = 2; x <= 26; x += 2)
+			await helper.svgClick(svg, x, randY + 1);
+
+		await helper.delay(1 * 500);//wait for signalR to settle in (?)
+
+		const winMessageVisible = await p2.page.locator('div.modal-body', { hasText: 'And the winner is... blue.' }).isVisible();
+		if (!winMessageVisible) {
+			await helper.svgClick(svg, 38, randY + 3);//somehow we are lacking last point, put it just to be sure
+		}
+
+		await helper.verifyWin(p2, 'And the winner is... blue.');
+	});
 });
